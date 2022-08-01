89b9422aa7f15f726ffff7f25dda588f525d6564
wangxincheng <xincheng.wang@hand-china.com>
2022-08-01 09:21:36 +0800

    TEST

---
+++ src/components/TodoList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="text" v-model="title" @keydown.enter="addTodo" />
+    <button v-if="active < all" @click="clear">清理</button>
+    <ul v-if="todos.length">
+      <li v-for="todo in todos">
+        <input type="checkbox" v-model="todo.done" />
+        <span :class="{ done: todo.done }"> {{ todo.title }}</span>
+      </li>
+    </ul>
+    <div v-else>暂无数据</div>
+    <div>
+      全选<input type="checkbox" v-model="allDone" />
+      <span> {{ active }} / {{ all }} </span>
+    </div>
+    x轴：{{ x }} y轴：{{ y }}
+    <h1 @click="add">{{ count }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import useTodos from "../composition/useTodos.js";
+import useMouse from "../composition/useMouse.js";
+let count = ref(1);
+let color = ref("red");
+function add() {
+  count.value++;
+  color.value = Math.random() > 0.5 ? "blue" : "red";
+}
+let { title, todos, addTodo, clear, active, all, allDone } = useTodos();
+let { x, y } = useMouse();
+</script>
+
+<style>
+h1 {
+  color: v-bind(color);
+}
+</style>
---
+++ src/pages/home.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>这是首页</h1>
+  <TodoList />
+  <h1 @click="toggle1">全屏化</h1>
+  <Rate :value="score" />
+  <Rate :value="4" theme="red"></Rate>
+  <Rate :value="1" theme="green"></Rate>
+</template>
+
+<script setup>
+import TodoList from "../components/TodoList.vue";
+import Rate from "../components/Rate.vue";
+import { useFullscreen } from "@vueuse/core";
+import { ref } from "vue";
+const { toggle } = useFullscreen();
+function toggle1() {
+  console.log("测试全屏化");
+  toggle();
+}
+let score = ref(3);
+</script>
